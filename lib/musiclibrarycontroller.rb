93edf55cd314db1c7997d36e868f66485fa7a8e1
Austin Powell <austinpowell58@gmail.com>
2018-08-28 23:50:32 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -16,7 +16,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    user_input = gets.
+    user_input = gets.chomp
     
     
   end 
