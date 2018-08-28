77bd0c0dc9037fdd6a43362f8fd59475b1bb6e9c
Austin Powell <austinpowell58@gmail.com>
2018-08-28 23:47:24 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -13,6 +13,7 @@ class MusicLibraryController
     puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
       expect($stdout).to receive(:puts)"To list all of your songs, enter 'list songs'.")
       expect($stdout).to receive(:puts).with("To list all of the artists in your library, enter 'list artists'.")
       expect($stdout).to receive(:puts).with("To list all of the genres in your library, enter 'list genres'.")
