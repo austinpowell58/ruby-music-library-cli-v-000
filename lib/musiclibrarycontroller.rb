784f0559d3863398de9b48621db7673a529acdc7
Austin Powell <austinpowell58@gmail.com>
2018-08-28 23:46:19 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -8,7 +8,17 @@ class MusicLibraryController
   
   def call
     puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of your songs, enter 'list songs'." expect($stdout).to receive(:puts).with("Welcome to your music library!")
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts 
+      expect($stdout).to receive(:puts)"To list all of your songs, enter 'list songs'.")
+      expect($stdout).to receive(:puts).with("To list all of the artists in your library, enter 'list artists'.")
+      expect($stdout).to receive(:puts).with("To list all of the genres in your library, enter 'list genres'.")
+      expect($stdout).to receive(:puts).with("To list all of the songs by a particular artist, enter 'list artist'.")
+      expect($stdout).to receive(:puts).with("To list all of the songs of a particular genre, enter 'list genre'.")
+      expect($stdout).to receive(:puts).with("To play a song, enter 'play song'.")
+      expect($stdout).to receive(:puts).with("To quit, type 'exit'.")
+      expect($stdout).to receive(:puts).with("What would you like to do?")
     
   end 
     
